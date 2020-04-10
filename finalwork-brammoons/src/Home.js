8564dc761ac36ff07a4d69075c6fe5ace6192c56
Bram <bram.moons@i8c.be>
2020-04-10 11:34:15 +0200

    first axios functions with localhost ip

--- finalwork-brammoons/src/Home.js
+++ finalwork-brammoons/src/Home.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
+
+    constructor() {
+        this.getAllWalks = this.getAllWalks.bind(this);
+        this.getAllAnimals = this.getAllAnimals.bind(this);
+        this.getAllShelters = this.getAllShelters.bind(this);
+    }
+
+    componentWillMount() {
+        this.getAllWalks();
+        this.getAllAnimals();
+        this.getAllShelters();
+    }
+
+    getAllWalks() {
+        axios.get("https://172.0.0.1/Wandeling/GetAll");
+    }
+    
+    getAllAnimals() {
+        axios.get("https://172.0.0.1/Dier/GetAll");
+    }
+
+    getAllShelters() {
+        axios.get("https://172.0.0.1/Asiel/GetAll");
+    }
 
     render() {
         return (
